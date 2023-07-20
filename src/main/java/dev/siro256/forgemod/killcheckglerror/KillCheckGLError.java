35ed50eef16221d03ac9eebfd8dde0f228ccbbf9
Siro_256 <siro@siro256.dev>
2023-07-21 02:39:27 +0900

    Create main mod class

---
+++ src/main/java/dev/siro256/forgemod/killcheckglerror/KillCheckGLError.java
@@ -0,0 +1,6 @@
+package dev.siro256.forgemod.killcheckglerror;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "@modId@", name = "@modName@", version = "@modVersion@", clientSideOnly = true)
+public class KillCheckGLError {}
